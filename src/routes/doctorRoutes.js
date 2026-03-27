71e3b14ef30dc427bab040eb9c397d199d0f547f
GJHKolwane <vts.ceo@outlook.com>
2026-03-27 23:29:29 +0200

    Update doctorRoutes.js

--- src/routes/doctorRoutes.js
+++ src/routes/doctorRoutes.js
@@ -1,7 +1,8 @@
 import fs from "fs";
 import path from "path";
 import crypto from "crypto";
-
+import express from "express";
+const router = express.Router();
 import {
   processCaseState,
   enforceTransition,
