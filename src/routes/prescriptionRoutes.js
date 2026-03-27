69f2c5a76a94190268ca168f9a0ad21e8f759a12
GJHKolwane <vts.ceo@outlook.com>
2026-03-27 23:38:26 +0200

    Update prescriptionRoutes.js

--- src/routes/prescriptionRoutes.js
+++ src/routes/prescriptionRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-const router = express.Router();
+
 import { createPrescriptionHandler } from "../controllers/prescriptionController.js";
 
 const router = express.Router();
