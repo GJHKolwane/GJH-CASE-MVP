c335629a645073b4f784d64d655e41f5c77f9124
GJHKolwane <vts.ceo@outlook.com>
2026-03-28 18:58:32 +0200

    Create db.js

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "gjhealth",
+  password: "yourpassword",
+  port: 5432,
+});
+
+export const query = (text, params) => pool.query(text, params);
+
+export default pool;
