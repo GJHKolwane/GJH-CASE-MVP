ecc3d0d63199f7e4acdf59fd12b2d3c47203ab35
GJHKolwane <vts.ceo@outlook.com>
2026-03-30 21:35:02 +0200

    Update case.controller.ts

--- src/controllers/case.controller.ts
+++ src/controllers/case.controller.ts
@@ -1,13 +1,11 @@
 import { processCase } from "../services/case.service.js";
 
-export async function handleCase(req: any, res: any) {
+export async function handleCase(req, res) {
   try {
     const result = await processCase(req.body);
-
     res.json(result);
-  } catch (error: any) {
+  } catch (error) {
     console.error("Case Processing Error:", error.message);
-
     res.status(500).json({
       message: "Case processing failed",
     });
