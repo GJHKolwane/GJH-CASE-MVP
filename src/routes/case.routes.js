4085ca43ba32ea5dfca983dfecb335de2c0aafb8
GJHKolwane <vts.ceo@outlook.com>
2026-04-16 16:07:03 +0200

    Delete src/routes/case.routes.js

--- src/routes/case.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { handleCase } from "../controllers/case.controller.js";
-
-const router = express.Router();
-
-router.post("/process", handleCase);
-
-export default router;
